04d5ae7f962e7fc0373440f425d63afbec06c5b7
ManuChitroju <37648294+ManuChitroju@users.noreply.github.com>
2021-07-17 15:33:33 +0530

    Update settings.py

--- django_hello/settings.py
+++ django_hello/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+#ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = ["mbgbs.com"]
 
 
 # Application definition
