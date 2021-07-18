9887514e28cdd845f81da63042413c571bee6b02
Manu Brahmam Chitroju <37648294+ManuChitroju@users.noreply.github.com>
2021-07-18 07:42:52 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-#def hello(request):
-#   return HttpResponse("Hello, World!... This is Manu")
-
 def hello(request):
-   return render(request, 'personal/first.html')
+   return HttpResponse("Hello, World!... This is Manu")
+
+#def hello(request):
+#   return render(request, 'personal/first.html')
